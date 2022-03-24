b6dd9107b64101b7ba1976b521d768269494e678
iahtnavmahp <thaisaker114@gmail.com>
2022-03-24 09:50:13 +0700

    first
    ...

---
+++ Training_IOS_2/Training_IOS_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Training_IOS_2
+//
+//  Created by Pham Van Thai on 24/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
